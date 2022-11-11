f6811eb246b78288960aecf200a26c0e653a634a
pr_rockz <priyanshuranjan2806@gmail.com>
2022-11-11 22:25:39 +0530

    Modified the GoodDay file

--- GoodDay.sh
+++ GoodDay.sh
@@ -2,7 +2,14 @@
 
 user=$(whoami)
 date=$(date)
+presentDiro=$(pwd)
+privateIP=$(ipconfig getifaddr en0)
 
 echo "You are currently logged in as $user."
 echo "Today's date is $date"
-ipaddr=$(curl ifconfig.me --output -)
+echo "Your present working directory is $presentDiro"
+echo ""
+echo "Your private or local IP Address is : $privateIP"
+echo ""
+echo "Your public or internet IP address is :"
+echo $(curl ifconfig.co)
