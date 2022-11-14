9d9008aa4ed7c73c033a0e4143e2d1a43e687a86
pr_rockz <priyanshuranjan2806@gmail.com>
2022-11-14 23:45:00 +0530

    Updated usrcompinfo file

--- UsrCompInfo.sh
+++ UsrCompInfo.sh
@@ -6,11 +6,27 @@ presentDiro=$(pwd)
 privateIP=$(ipconfig getifaddr en0)
 
 echo "You are currently logged in as $user."
+printf "\n"
+
 echo "Today's date is $date"
 echo "Your present working directory is $presentDiro"
-echo ""
+printf "\n"
+
+echo "Now some Software info about Local Machine"
+read -p "For detailed info type 'd' and for compact 
+info type 'c': " info
+
+if [$info="d"]||[$info="D"];
+then
+system_profiler SPSoftwareDataType
+else
+sw_vers
+printf "\n"
+fi
+
+echo $info
 echo "Your private or local IP Address is : $privateIP"
-echo ""
+printf "\n"
 echo "Your public or internet IP address is : $(wget -qO- http://ipecho.net/plain | xargs echo)"
 
 
